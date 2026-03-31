71c8e60c0a1e01959921ed93bc620fa189a95a68
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-31 19:07:13 +0500

    Configured the mongoDB also added the environment variables in the app.

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import configs from "./config.js";
+
+async function connectDB() {
+  try {
+    if (!configs.MONGO_URI) {
+      throw new Error("Mongo URI is not defined");
+    }
+    await mongoose.connect(configs.MONGO_URI + "/auth-sys");
+    console.log("DB connected successfully!");
+  } catch (error) {
+    console.log("Error connecting DB", error);
+  }
+}
+
+export default connectDB;
