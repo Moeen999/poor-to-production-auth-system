58373008177cbcfb8cf1b864d73c9e5b2a09cb37
Muhammad Moeen <mooenmalik374@gmail.com>
2026-04-01 11:45:04 +0500

    added the controller to get the data of the user.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
-import express from "express"
+import express from "express";
 import * as authController from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register",authController.register)
+authRouter.post("/register", authController.register);
+authRouter.get("/get-me", authController.getMe);
 
 export default authRouter;
